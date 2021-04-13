a9c772d815ee5b53407ba6cfa72c8349d94d4542
jakubKuznik <kuznja16@gmail.com>
2021-04-13 09:58:14 +0200

    Add files via upload
    Finilazing

--- uart.vhd
+++ uart.vhd
@@ -1,7 +1,6 @@
 -- uart.vhd: UART controller - receiving part
 -- Author(s): xkuzni04 Jakub Kuzník
---
-
+-- Soubor funguje UART a jeho stavy uklada do uart.fsm.vhd
 
 library ieee;
 use ieee.std_logic_1164.all;
@@ -21,13 +20,13 @@ end UART_RX;
 -------------------------------------------------
 architecture behavioral of UART_RX is
   
-signal cnt      : std_logic_vector(4 downto 0);
-signal cnt_2    : std_logic_vector(3 downto 0);
+signal cnt_clck      : std_logic_vector(4 downto 0); -- v cnt_clck vzdy pocitam 16 clck signalu
+signal cnt_bit    : std_logic_vector(3 downto 0); -- v cnt_bit pocitam bity
 
-signal stopb_en    : std_logic;
-signal stopb_en_in : std_logic;
+signal stopb_en    : std_logic; 
+signal stopb_en_in : std_logic; -- stop bit state enabl e
 
-signal vd_ok    : std_logic;
+signal vd_ok    : std_logic; -- Valid data state enable 
 
 signal cnt_en   : std_logic; -- Indikuje   ze mam inkrementovat cnt
 signal cnt_2_en : std_logic; -- Indikator inkremetace cnt_2
@@ -40,18 +39,17 @@ begin
     -- Design under test
     STATE_MACHINE: entity work.UART_FSM(behavioral)
     port map (
-        CLK 	     => clk,
-        RST 	     => rst,
-        DIN 	     => din,
-        CNT 	     => cnt,
-        CNT_2 	   => cnt_2,
-        RX_EN     => rx_en,
-        CNT_EN    => cnt_en,
-        CNT_2_EN  => cnt_2_en,
-        DOUT_VLD  => DVLD,
-        SB_EN     => stopb_en, --sb == stop bit 
-        SB_EN_IN  => stopb_en_in,
-        VD_OK     => vd_ok
+        CLK 	         => clk,
+        RST 	         => rst,
+        DIN 	         => din,
+        CNT_CLCK      => cnt_clck,
+        CNT_BIT 	     => cnt_bit,
+        RX_EN         => rx_en,
+        CNT_CLCK_EN   => cnt_en,
+        CNT_BIT_EN    => cnt_2_en,
+        VALID         => DVLD,
+        SB_EN         => stopb_en, --sb == stop bit 
+        STOPB_EN_IN   => stopb_en_in
     );
 
 
@@ -60,50 +58,50 @@ begin
     
         if rising_edge(CLK) then
          
+         
+          --COUNTER SET TO 0 WHEN STATE IS NOT RECEIVE ENABLE 
           if rx_en = '0' then
-            cnt_2 <= "0000";
+            cnt_bit <= "0000";
           end if; 
-          
+          ------------------
+        
+          ---COUNTER INCEMENT 
+          if cnt_en = '1' then -- if we are in state where we count 
+              cnt_clck <= cnt_clck + 1;  -- increment counter 
+           else                -- if we are not at this state alway set it to 0
+              cnt_clck <= "00000";          
+          end if;
+          -----------------
+                
+          --SENDING VALID BIT 
           if DVLD = '1' then
             DOUT_VLD <= '1';
-            vd_ok <= '1';
           else
             DOUT_VLD <= '0';
-            vd_ok <= '0';
           end if;
-            
-          if cnt_en = '1' then -- if we are in state where we count 
-              cnt <= cnt + 1;  -- increment counter 
-           else                -- if we are not at this state alway set it to 0
-              cnt <= "00000";          
-          end if;
-          
-          
+          -----------------
+               
+         --WAIT FOR STOP BIT
           if stopb_en = '1' then
-            report "hovno";
-            if DIN = '1' then
-              
+            if DIN = '1' then 
               stopb_en_in <= '1'; -- FSM zmeni stav z 4 -> 5
             end if; 
-           end if;  
-
-
+           end if; 
+           
            --READ_DATA_STATE
            if rx_en = '1' then
-              if cnt_2 = "0000"  then
-                cnt <= "00000";
-                DOUT(to_integer(unsigned(cnt_2))) <= DIN;
-                cnt_2 <= cnt_2 + 1;
-              else if cnt = "01111" then
-                cnt <= "00000";
-                DOUT(to_integer(unsigned(cnt_2))) <= DIN;
-                cnt_2 <= cnt_2 + 1;
+              if cnt_bit = "0000"  then
+                cnt_clck <= "00000";
+                DOUT(to_integer(unsigned(cnt_bit))) <= DIN;
+                cnt_bit <= cnt_bit + 1;
+              else if cnt_clck = "01111" then
+                cnt_clck <= "00000";
+                DOUT(to_integer(unsigned(cnt_bit))) <= DIN;
+                cnt_bit <= cnt_bit + 1;
               end if;     
-          end if;                
-                         
-           
-           
-           
+          end if;        
+          
+                             
         end if;
       end if;
   end process;    
--- uart_fsm.vhd
+++ uart_fsm.vhd
@@ -1,6 +1,6 @@
 -- uart_fsm.vhd: UART controller - finite state machine
 -- Author(s):Jakub Kuzník xkuzni04 
---
+-- Tento soubor je FSM kde ukladam a menim stavy 
 
 
 --soubor konecného automatu
@@ -11,20 +11,18 @@ use ieee.std_logic_1164.all;
 -------------------------------------------------
 entity UART_FSM is
 port( --zde definujeme jaké má entinta porty 
-   CLK     :    in std_logic;
-   RST     :    in std_logic;
-   DIN     :    in std_logic;                    -- 4 3 2 1 0 == 32 combination
-   CNT     :    in std_logic_vector(4 downto 0); -- pole bitu s indexy - - - - - 
-   CNT_2   :    in std_logic_vector(3 downto 0);  
-   SB_EN_IN :   in std_logic; --slouzi pro zmenu stavu z 4 -> 5
-   VD_OK   :   in std_logic; -- zmena stavu z 5 -> 1 
+   CLK         :  in std_logic;
+   RST         :  in std_logic;
+   DIN         :  in std_logic;                    -- 4 3 2 1 0 == 32 combination
+   CNT_CLCK    :  in std_logic_vector(4 downto 0); -- pole bitu s indexy - - - - - 
+   CNT_BIT     :  in std_logic_vector(3 downto 0);  
+   STOPB_EN_IN :  in std_logic; --slouzi pro zmenu stavu z 4 -> 5
    
-   RX_EN    : out std_logic;  -- read_data state
-   SB_EN    : out std_logic;  -- stop bit state 
-   --VD_EN    : out std_logic;  -- valid data state 
-   CNT_EN   : out std_logic;
-   CNT_2_EN : out std_logic;
-   DOUT_VLD : out std_logic   -- data valid state 
+   RX_EN         : out std_logic;  -- read_data state
+   SB_EN         : out std_logic;  -- stop bit state 
+   CNT_CLCK_EN   : out std_logic;  -- enable cnt_clck
+   CNT_BIT_EN    : out std_logic;  -- enable cnt_bti
+   VALID         : out std_logic   -- data valid state 
    );
 end entity UART_FSM;
 
@@ -43,41 +41,39 @@ begin
   RX_EN   <='1'   when state = READ_DATA else '0';
   SB_EN   <='1'   when state = WAIT_STOP_BIT else '0';
   
-  CNT_EN  <='1'   when state = COUNT_UNTIL_F_B or state = READ_DATA or state = WAIT_STOP_BIT else '0';
-  CNT_2_EN <='1'  when state = READ_DATA else '0';
-  DOUT_VLD <='1'  when state = VALID_DATA else '0';
+  CNT_CLCK_EN  <='1'   when state = COUNT_UNTIL_F_B or state = READ_DATA or state = WAIT_STOP_BIT else '0';
+  CNT_BIT_EN <='1'  when state = READ_DATA else '0';
+  VALID <='1'  when state = VALID_DATA else '0';
   
   process (CLK) begin -- proces se vyvola kdykoliv se zmeni clock 
    if rising_edge(CLK) then
       if RST = '1' then 
         state <= WAIT_START_BIT;
-        else
+      else
            case state is
+              -- 1. STATE WAIT FOR START  BIT 
               when WAIT_START_BIT   => if DIN = '1' then
                                             state <= WAIT_START_BIT;
                                         else 
                                           state <= COUNT_UNTIL_F_B;
-                                          
                                         end if;
-              when COUNT_UNTIL_F_B  => if CNT = "10111" then
-                  -- cekam do 24 abych se dostal do prostred signalu
+                                        
+              -- 2. STATE COUNT CLCK UNTIL MEASURING FIRST INPUT BIT                           
+              when COUNT_UNTIL_F_B  => if CNT_CLCK = "10111" then -- cekam do 24 abych se dostal do prostred signalu
                                              state <= READ_DATA;
                                         end if;
-              when READ_DATA        => if CNT_2 = "1000" then --when i have read 8 bits
-                                            state <= WAIT_STOP_BIT;
-                                            
+              -- 3. STATE READ 8 INPUT BITS                          
+              when READ_DATA        => if CNT_BIT = "1000" then --when i have read 8 bits
+                                            state <= WAIT_STOP_BIT;     
                                        end if;     
-                                       
-              when WAIT_STOP_BIT    => if SB_EN_IN = '1' then
+              -- 4. STATE WAIT UNTIL STOP BI                                             
+              when WAIT_STOP_BIT    => if STOPB_EN_IN = '1' then
                                             state <= VALID_DATA; 
                                       end if;
+              -- 5. STATE SET VALID BIT                         
+              when VALID_DATA       => state <= WAIT_START_BIT;
                                       
-             when VALID_DATA       => if VD_OK = '1' then
-                                        state <= WAIT_START_BIT;
-                                      end if;
-                                      
-                                      
-              when others           => null;
+             when others           => null;
             end case;         
       end if;
    end if;   
